095b18a6450edf48beeb66cad2776de5eeb9e5c2
vkochano <vladimir.kochanov.g@gmail.com>
2018-10-15 22:51:29 -0400

    added  enum templates

---
+++ Generator/renderers/common.py
@@ -0,0 +1,10 @@
+class Renderer:
+    def __init__(self, model, config):
+        self.model = model
+        self.config = config
+
+    def generateRevision(self):
+        return "enum {{ SCHEMA_REVISION = {REVISION} }}".format(REVISION = self.config.REVISION)
+
+    def genCharList(self, name):
+        return ','.join(["'" + c + "'" for c in name])
